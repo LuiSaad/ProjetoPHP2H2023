eb5b692c063300bcd77aa613ea33efb88db28015
Luisa Mohamed Saad <100882880+LuiSaad@users.noreply.github.com>
2023-02-27 20:22:19 -0300

    Add files via upload

--- helloworld.php
+++ helloworld.php
@@ -4,6 +4,17 @@
 echo "Olá mundo! <br>";
 echo "Estou criando meu sistema.";
 
+echo "Exemplos de comentários";
+//Estilo de comentários de linhas
+
+/*Estilo de
+comentários de blocos
+*/
+
+#Estilo de comentário de linhas
+
+#echo 'Exemplo de comentário’
+
 
 
 ?>

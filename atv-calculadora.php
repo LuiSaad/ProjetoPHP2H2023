7221ef5c761e5c01ae9947b9f3904b6927f16483
Cyntia Lino <100882616+Linocyntia@users.noreply.github.com>
2023-04-05 11:29:45 -0300

    Add files via upload

--- atv-calculadora.php
+++ atv-calculadora.php
@@ -26,39 +26,45 @@ return $c;
 
 	$n1 = 8;
  	$n2 = 4;
- 	$op = 2;
+ 	$op = 1;
 
  	switch ($op) {
-	case 1: 
+	case 1:
 		echo 'Operação escolhida: adição';
-		somarvalor($n1,$op,$n2);
+		echo "<br> Primeiro número escolhido: $n1";
+		echo "<br> Segundo número escolhido: $n2";
+		echo "<br> O resultado do cálculo é: " .somarvalor($n1,$op,$n2);
 		
 		break;
 	case 2:
 		echo 'Operação escolhida: subtração';
-		subtrairvalor($n1,$op,$n2);
+		echo "<br> Primeiro número escolhido: $n1";
+		echo "<br> Segundo número escolhido: $n2";
+		echo "<br> O resultado do cálculo é: " .subtrairvalor($n1,$op,$n2);
 
 		break;
 	case 3: 
 		echo 'Operação escolhida: multiplicação';
-		multiplicarvalor ($n1,$op,$n2);
-		
+		echo "<br> Primeiro número escolhido: $n1";
+		echo "<br> Segundo número escolhido: $n2";
+		echo "<br> O resultado do cálculo é: " .multiplicarvalor($n1,$op,$n2);
 
 		break;
 	case 4: 
 		echo 'Operação escolhida: divisão';
-		dividirvalor ($n1,$op,$n2);
+		echo "<br> Primeiro número escolhido: $n1";
+		echo "<br> Segundo número escolhido: $n2";
+		echo "<br> O resultado do cálculo é: " .dividirvalor($n1,$op,$n2);
+
 		
 	
 		break;
 
 
 	default: 
-		echo "Operação";
+		echo "Operação inválida";
 		break;
 	}
 	
-	echo "<br> Primeiro número escolhido: $n1";
-	echo "<br> Segundo número escolhido: $n2";
-	echo "<br> O resultado do cálculo é: " .subtrairvalor($n1,$op,$n2);
+	
  ?>

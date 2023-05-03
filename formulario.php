dda82a0bfaa200821344fd37104a06b3747763dd
Luisa Mohamed Saad <100882880+LuiSaad@users.noreply.github.com>
2023-05-03 20:16:34 -0300

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Exemplo Formulário HTML</title>
+</head>
+<body>
+
+	<form>
+		<input type="text">
+		<input type="submit" value="ENVIAR">
+
+	</form>
+	
+</body>
+</html>
